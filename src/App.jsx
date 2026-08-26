f3ae6cc417f01991c83d2a351ddc7e348fb30ef4
ridu101 <ridu116540@gmail.com>
2026-08-26 20:14:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import SimpleFrom from './components/SimpleForm/SimpleFrom'
+
+function App() {
+
+
+  return (
+    <>
+     
+          <h1>Get started</h1>
+          <h1>Welcome to React Forms</h1>
+
+          <SimpleFrom></SimpleFrom>
+          
+    </>
+  )
+}
+
+export default App

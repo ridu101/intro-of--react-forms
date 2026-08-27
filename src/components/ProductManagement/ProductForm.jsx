ac3f51a67962feb3d71dc3f64181a239a08e45b1
ridu101 <ridu116540@gmail.com>
2026-08-27 20:11:51 +0600

    new product added

--- src/components/ProductManagement/ProductForm.jsx
+++ src/components/ProductManagement/ProductForm.jsx
@@ -1,9 +1,27 @@
 // import React from 'react';
 
 const ProductForm = () => {
+    const handleProductOnSubmit= e =>{
+        e.preventDefault();
+
+        const productName= e.target.name.value;
+        const productPrice= e.target.price.value;
+        const productQuantity= e.target.quantity.value;
+
+        console.log(productName,productPrice,productQuantity)
+    }
     return (
         <div>
-            
+            <h3>Add to Cart a Product</h3>
+            <form onSubmit={handleProductOnSubmit}>
+                <input type="text" name="name"  placeholder="Product Name" />
+                <br />
+                <input type="text" name="price"  placeholder="Product Price" />
+                <br />
+                <input type="text" name="quantity"  placeholder="Product Quantity" />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
